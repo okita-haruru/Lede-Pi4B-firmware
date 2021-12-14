1f5ff2e5b9befb9a43407bea06ebd6a4edd2d540
NullPointer <keriteal985@gmail.com>
2021-12-14 17:45:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,5 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
+sed -i 's/192.168.1.1/192.168.39.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
