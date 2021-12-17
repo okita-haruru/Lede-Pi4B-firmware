8e64cc1d53a373f00a9bcb2342e0faf34cddf2c0
NullPointer <keriteal985@gmail.com>
2021-12-17 15:51:27 +0800

    Update post-feed.sh

--- post-feed.sh
+++ post-feed.sh
@@ -10,13 +10,19 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 修改默认ip
 sed -i 's/192.168.1.1/192.168.39.1/g' package/base-files/files/bin/config_generate
-# Modify default theme
+# 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 修改lede默认的腾讯云镜像为清华镜像
+sed -i 's#mirrors.cloud.tencent.com/lede#mirrors.tuna.tsinghua.edu.cn/openwrt#g' package/lean/default-settings/files/zzz-default-settings
 
 # passwall比openclash好用所以先注释
 # git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+
+# 配置argon主题
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
