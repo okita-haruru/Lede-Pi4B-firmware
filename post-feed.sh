41e80c7893212deacc0a3feb581c17d4011c1030
NullPointer <keriteal985@gmail.com>
2021-12-17 21:30:53 +0800

    Update post-feed.sh

--- post-feed.sh
+++ post-feed.sh
@@ -17,12 +17,14 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 # 修改lede默认的腾讯云镜像为清华镜像
 sed -i 's#mirrors.cloud.tencent.com/lede#mirrors.tuna.tsinghua.edu.cn/openwrt#g' package/lean/default-settings/files/zzz-default-settings
 
-# passwall比openclash好用所以先注释
-# git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+# Mentohust 校园网上网
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 
 # 配置argon主题
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+# 安装一些额外的包
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
