4dd9ecbd36ea75a63b3e769a6dc71a5c15873983
Keriteal <keriteal985@gmail.com>
2021-12-18 22:51:25 +0800

    update shell

--- post-feed.sh
+++ post-feed.sh
@@ -8,12 +8,13 @@ LEDE_ROOT=$(pwd)
 # 修改默认ip
 sed -i 's/192.168.1.1/192.168.39.1/g' package/base-files/files/bin/config_generate
 # 修改lede默认的腾讯云镜像为清华镜像
-sed -i 's#mirrors.cloud.tencent.com/lede#mirrors.tuna.tsinghua.edu.cn/openwrt#g' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's#mirrors.cloud.tencent.com/lede#mirrors.tuna.tsinghua.edu.cn/openwrt#g' package/lean/default-settings/files/zzz-default-settings
 # 修改主机名
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='MikuWrt'' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/OpenWrt /MikuWrt /g'
 
 mkdir package/community
+echo "Entering package/community"
 pushd package/community
 
 # 解锁网易云音乐
@@ -28,14 +29,14 @@ git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust.git
 [ -e ../lean/luci-theme-argon ] && rm -rf ../lean/luci-theme-argon
 git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' $LEDE_ROOT/feeds/luci/collections/luci/Makefile
 [ -e $LEDE_ROOT/data/argon-background.jpg ] && mv $LEDE_ROOT/data/argon-background.jpg luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # 在线设备列表
 git clone --depth=1 https://github.com/Kerite/luci-app-onliner.git
 
 # 关机
-git clone https://github.com/esirplayground/luci-app-poweroff.git
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff.git
 
 #ddnsto和易有云
 svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
@@ -46,4 +47,12 @@ svn co https://github.com/linkease/nas-packages/trunk/network/services/linkease
 # 应用过滤
 git clone --depth=1 https://github.com/destan19/OpenAppFilter.git
 
+# luci-app-ssr-plus
+git clone --depth=1 https://github.com/fw876/helloworld.git
+
+# luci-app-vssr
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
+git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
+
+echo "Leving package/community"
 popd
