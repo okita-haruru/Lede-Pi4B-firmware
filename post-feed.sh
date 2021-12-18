a9de82740f1003e674394af147351dde34a7fe91
Keriteal <keriteal985@gmail.com>
2021-12-18 19:21:33 +0800

    Update script

--- post-feed.sh
+++ post-feed.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# 本脚本在feeds update和feeds install后被执行
+
+# ./scripts/feeds update -a && ./scripts/feeds install -a
+# 请勿取消上面行的注释
 
 # 修改默认ip
 sed -i 's/192.168.1.1/192.168.39.1/g' package/base-files/files/bin/config_generate
@@ -15,19 +17,21 @@ git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.gi
 
 # Mentohust 校园网上网
 git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git mentohust
-git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust.git luci-app-mentohust
+git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust.git
 
 # 配置argon主题
-rm -rf ../lean/luci-theme-argon
-git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git ../lean/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
+[ -e ../lean/luci-theme-argon ] && rm -rf ../lean/luci-theme-argon
+git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git
 sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci/Makefile
+[ -e ../../files/www/luci-static-argon-backgrund/bg1.*] && \ cp -i ../../files/www/luci-static-argon-backgrund/bg1.* \
+ luci-theme-argon/htdocs/luci-static/argon/img/
 
 # 在线设备列表
 git clone --depth=1 https://github.com/Kerite/luci-app-onliner.git
 
 # 关机
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+git clone https://github.com/esirplayground/luci-app-poweroff.git
 
 #ddnsto和易有云
 svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
